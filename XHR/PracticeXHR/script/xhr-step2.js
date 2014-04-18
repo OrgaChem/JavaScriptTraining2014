4b7aadd7c89e512a41eeea078ecb3b16074de00e
OrgaChem <orga.chem.job@gmail.com>
2014-04-18 12:50:44 +0900

    Switch to use jQuery

--- XHR/PracticeXHR/script/xhr-step2.js
+++ XHR/PracticeXHR/script/xhr-step2.js
@@ -1,33 +1,21 @@
-// DOMを操作する場合はページの読み込みを待つ
-window.onload = function() {
+// 自分のhostnameに変更する
+var path = 'http://localhost:8080/result/';
 
-    // 自分のhostnameに変更する
-    var path = "http://HOSTNAME:PORT/result/";
+var sel = $('#bloodTypeBox');
+var resArea = $('#resultArea');
 
-    // xhrの準備
-    @@@@
-    @@@@
+sel.on('change', function() {
+  var bloodType = $(this).val();
+  if (bloodType === 'default') {
+    return;
+  }
+  var url = path + 'type-' + bloodType + '.txt';
 
-    var select = document.getElementById(@@@@);
-    select.onchange = function() {
-        // 選択された値を取得する
-        var selectedOption = this.options[ this.selectedIndex ];
-        var selectedBloodType = selectedOption.value;
-        //alert (selectedBloodType);
-
-        if (selectedBloodType !== "default") {
-            // urlを作成し、xhrでtxtファイルを取得する
-            var url = path + "type-" + @@@@ + ".txt";
-            @@@@
-            @@@@
-        }
-    }
-
-    function drawResult() {
-        if((xhr.readyState === 4) && (xhr.status === 200)) {
-            //alert(xhr.responseText);
-            var resultArea = document.getElementById(@@@@);
-            resultArea.textContent = @@@@;
-        }
-    }
-}
+  $.get(url)
+    .done(function(data) {
+      resArea.text(data);
+    })
+    .fail(function(err) {
+      console.error(err);
+    });
+});
